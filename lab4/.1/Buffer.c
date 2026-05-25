fc222e5564a76ac8850ebd0428220b18888510c9
XeN9GeN <k.vanin@g.nsu.ru>
2026-05-25 23:02:40 +0700

    Update Buffer.c

--- lab4/.1/Buffer.c
+++ lab4/.1/Buffer.c
@@ -10,33 +10,34 @@ int create_buffer() {
     int size = BUFF_SIZE;
     const char* phrase = "hello world";
 
-  
+
     char* buff1 = (char*)malloc(size);
-    if (buff1 == NULL) {
-        perror("Error of allocation memory: buff1\n");
-        return ERROR_RET_VAL;
+    if (buff1 != NULL) {
+        strcpy(buff1, phrase);
+        printf("Buffer1 phrase before free: %s\n", buff1);
+        free(buff1);
+
+        printf("Buffer1 phrase after free (UB): %s\n", buff1);
     }
-    strcpy(buff1, phrase);
-
-    printf("Buffer1 phrase before free: %s\n", buff1);
-    free(buff1);
-    printf("Buffer1 phrase after free: %s\n", buff1);
-
+    else {
+        perror("Error of allocation memory: buff1\n");
+    }
 
     
     char* buff2 = (char*)malloc(size);
-    if (buff2 == NULL) {
-        perror("Error of allocation memory: buff2\n");
-        return ERROR_RET_VAL;
+    if (buff2 != NULL) {
+        strcpy(buff2, phrase);
+        printf("Buffer2 phrase before free: %s\n", buff2);
+
+        char* cursor_buff2 = buff2 + (size / 2);
+        free(cursor_buff2);
+        printf("Buffer2 phrase after free: %s\n", buff2);
+        free(buff2);
     }
-    strcpy(buff2, phrase);
+    else {
+        perror("Error of allocation memory: buff2\n");
+    }
 
-    printf("Buffer2 phrase before free: %s\n", buff2);
-
-    char* cursor_buff2 = buff2 + (size / 2);
-    free(cursor_buff2); 
-
-    printf("Buffer2 phrase after free: %s\n", buff2);
     return SUCCESS;
 }
 
